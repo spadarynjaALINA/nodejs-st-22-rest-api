b12ed6c57b935dc62a9869b04a3e7de1d24a5cab
spadarynjaALINA <alinagusarik@gmail.com>
2022-07-27 11:03:46 +0300

    feat: add users model and create table for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/models/users.model';
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    UsersModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nest',
+      models: [User],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
 import { InMemoryUsersStore } from './store/user.storage';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './models/users.model';
 
 @Module({
   providers: [
@@ -12,5 +14,6 @@ import { InMemoryUsersStore } from './store/user.storage';
     },
   ],
   controllers: [UsersController],
+  imports: [SequelizeModule.forFeature([User])],
 })
 export class UsersModule {}
