14e8b130c0a425a401c12435e5259654c8622a64
spadarynjaALINA <alinagusarik@gmail.com>
2022-08-05 18:02:02 +0300

    feat:add returns for user service

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from '../dto/create-user.dto';
-import { UpdateUserDto } from '../dto/update-user.dto';
-import { IQuery } from '../interfaces/users.interfaces';
+
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from '../../../models/user';
 import { v4 as uuid } from 'uuid';
 import { Op } from 'sequelize';
+import { IQuery, IUser } from 'src/interfaces/users.interfaces';
+import { CreateUserDto } from 'src/dto/create-user.dto';
+import { UpdateUserDto } from 'src/dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User) private userRepository: typeof User) {}
 
-  async create(UserDto: CreateUserDto) {
+  async create(UserDto: CreateUserDto): Promise<IUser> {
     const newUser = {
       ...UserDto,
       id: uuid(),
@@ -21,20 +22,20 @@ export class UsersService {
     return user;
   }
 
-  async getAutoSuggestUsers(query: IQuery) {
+  async getAutoSuggestUsers(query: IQuery): Promise<IUser[]> {
     const users = await this.userRepository.findAndCountAll({
       where: { login: { [Op.substring]: query.loginSubstring || '' } },
       limit: query.limit || 15,
       order: [['login', 'ASC']],
     });
-    return users;
+    return users.rows;
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<IUser> {
     return await this.userRepository.findOne({ where: { id: id } });
   }
 
-  async update(updateUserDto: UpdateUserDto) {
+  async update(updateUserDto: UpdateUserDto): Promise<IUser> {
     const user = await this.userRepository.update(updateUserDto, {
       where: { id: updateUserDto.id },
       returning: true,
@@ -42,7 +43,7 @@ export class UsersService {
     return user[1][0];
   }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<IUser> {
     if ((await this.findOne(id)).isDeleted !== true) {
       const user = await this.userRepository.update(
         { isDeleted: true },
